fb979ddc4307a72be8bc69f40ea18d2e13410118
Nicolas Béhier-Dévigne <nbehierdevigne@gmail.com>
2016-05-29 11:26:54 +0200

    Add tests requirements

---
+++ tests/bootstrap.php
@@ -0,0 +1,4 @@
+<?php
+include_once __DIR__ . '/../vendor/bolt/bolt/tests/phpunit/bootstrap.php';
+define('EXTENSION_AUTOLOAD',  realpath(dirname(__DIR__) . '/vendor/autoload.php'));
+require_once EXTENSION_AUTOLOAD;
